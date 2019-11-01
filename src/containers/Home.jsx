9d56d4fac7413ece659bce6e577121bc7c99a99e
Edwin García <spark.com.co@gmail.com>
2019-11-01 16:08:49 -0500

    Crear nuestro archivo de Rutas

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -11,7 +11,7 @@ import useInitialState from '../hooks/useInitialState';
 
 const API = 'http://localhost:3000/initialState';
 
-const App = () => {
+const Home = () => {
   const initialState = useInitialState(API);
   return (
     <div className='App'>
@@ -46,4 +46,4 @@ const App = () => {
 
 };
 
-export default App;
+export default Home;
