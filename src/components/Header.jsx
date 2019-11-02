34d3c5cd0f499a4d582010782d6ffb5fba292b23
Edwin García <spark.com.co@gmail.com>
2019-11-01 21:36:20 -0500

    Validación para login logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import gravatar from '../utils/gravatar';
+import { logoutRequest } from '../actions';
 import '../assets/styles/components/Header.scss';
 import logo from '../assets/static/logo-platzi-video-BW2.png';
 import userIcon from '../assets/static/user-icon.png';
 
-const Header = ({ user }) => {
+const Header = ({ user, logoutRequest }) => {
   const hasUser = Object.keys(user).length > 0;
+  const handleLogout = () => {
+    logoutRequest({});
+  };
   return (
     <header className='header'>
       <Link to='/'>
@@ -21,8 +25,10 @@ const Header = ({ user }) => {
           <p>Perfil</p>
         </div>
         <ul>
-          <li><a href='/'>Cuenta</a></li>
-          <li><Link to='/login'>Iniciar Sesión</Link></li>
+          {hasUser && <li><a href='/'>{user.name}</a></li>}
+          {hasUser ?
+            <li><a href='#logout' onClick={handleLogout}>Cerrar Sesión</a></li> :
+            <li><Link to='/login'>Iniciar Sesión</Link></li>}
         </ul>
       </div>
     </header>
@@ -35,4 +41,8 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps, null)(Header);
+const mapDispatchToProps = {
+  logoutRequest,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
